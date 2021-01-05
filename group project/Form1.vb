66350c29d82f42ba59c9b2a5a30b24b1b29f6c7c
NURSYAMIMI IZZATI BINTI RAZALEE NURSYAMIMI IZZATI BINTI RAZALEE <2018422468@isiswa.uitm.edu.my>
2021-01-06 03:26:54 +0800

    add form2Add project files.

--- group project/Form1.vb
+++ group project/Form1.vb
@@ -6,4 +6,8 @@
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
         login.Show()
     End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
 End Class
